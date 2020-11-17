02d7804ee6cc142f4bf786dc991400751b176a50
Kim Francis Corros <corros121@gmail.com>
2020-11-18 06:41:46 +0800

    Add vuetify plugin

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Sample from './components/Sample.vue'
+import vuetify from '@/plugins/vuetify'
 
 export default {
   install: (Vue) => {
     Vue.component('Sample', Sample)
+    Vue.use(vuetify)
   }
 }
